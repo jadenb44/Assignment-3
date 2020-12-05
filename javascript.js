5ec6b9487865551dfd1e58ec218769777b45b069
jadenb44 <72582874+jadenb44@users.noreply.github.com>
2020-12-05 14:58:13 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,63 +1,96 @@
-(function update(){
-// gets which size is chosen from the radio buttons
-    function getRadio(form, name)
-    {
-        var val;
-        // get list of radio buttons with specified name
-        var options = form.elements[name];
+(function() {
         
-        // loop through list of radio buttons
-        for (var i=0, len=options.length; i<len; i++) 
-        {
-            if ( options[i].checked ) 
-            { // is radio checked?
-                val = options[i].value; // if so, hold its value in val
-                break; // and break out of for loop
-            }
-        }
-        return val; // return value of checked radio or undefined if none checked
-    }
-    var form = document.getElementById('newform'); 
-var size = form.elements['p_size'].value;
-for (var i = 0, len = size.length; i < len ; i++)
-{
-    size[i].onclick = form.elements['p_size_total'].value = parseInt(getRadio(form, 'p_size'));
-    var size_total = parseInt(form.elements['p_size_total'].value);
-localStorage.setItem("Size", size_total);
-Total(form);
-}
+				// format val to the n number of decimal places
+				function formatDecimal(val, n) {
+					n = n || 2;
+					var str = "" + Math.round ( parseFloat(val) * Math.pow(10, n) );
+					while (str.length <= n) {
+						str = "0" + str;
+					}
+					var pt = str.length - n;
+					return str.slice(0,pt) + "." + str.slice(pt);
+				}
+				//gets values from radio buttons 
+				function getRadioVal(form, name) {
+					var radios = form.elements[name];
+					var val;
+					
+					for (var i=0, len=radios.length; i<len; i++) {
+						if ( radios[i].checked == true ) {
+							val = radios[i].value;
+							break;
+						}
+						
+            	
+					}
+					return val;
+				}
 
-var pt = document.getElementById('p_toppings');
-for (var i = 0; i < pt.getElementsByTagName('input').length; i++){
-    if(pt.getElementsByTagName('input')[i] == 'checkbox'){
-        pt.getElementsByTagName('input')[i].onclick = Topping_total();
-    }
-}
+				
+				
 
-    function Topping_total(){
-        
-        var val = parseInt( form.elements['topping_total'].value );
-        
-        if ( this.checked == true ) {
-            val += parseInt(this.value);
-        } else {
-            val -= parseInt(this.value);
-        }
-        
-        form.elements['topping_total'].value = val;
-        Total(form);
-        localStorage.setItem("Toppings_Total",t_total);
-    }
-
-
-    
-
-    function Total(form){
-        var s_total = parseFloat( form.elements['p_size'].value );
-        var t_total = parseFloat( form.elements['topping_total'].value );
-        form.elements['total'].value =  (s_total + t_total );
-
-        localStorage.setItem("Total", s_total + t_total );
-    }
-
-})
+                  $(":checkbox").change(function() {
+                    var notChecked = [], checked = [];
+                    $(":checkbox").map(function() {
+                        this.checked ? checked.push(this.id) : notChecked.push(this.id);
+                    });
+                    localStorage.setItem("names " + checked[]);
+                    
+                
+                });
+// get total price for toppings by add all checked toppings
+				function getToppingsTotal(e) {
+					var form = this.form;
+					var val = parseFloat( form.elements['topping_total'].value );
+					
+					if ( this.checked == true ) {
+						val += parseFloat(this.value);
+					} else {
+						val -= parseFloat(this.value);
+					}
+					
+					form.elements['topping_total'].value = formatDecimal(val);
+					updatePizzaTotal(form);
+				}
+				// get the price for the pizza size selected
+				function getSizePrice(e) {
+					this.form.elements['p_size_total'].value = parseFloat( this.value );
+					updatePizzaTotal(this.form);
+				}
+	//calculates and stores all the details entered into localstorage
+				function updatePizzaTotal(form) {
+					var p_size_total = parseFloat( form.elements['p_size_total'].value );
+					var topping_total = parseFloat( form.elements['topping_total'].value );
+					form.elements['total'].value = formatDecimal( p_size_total + topping_total );
+ 
+					localStorage.setItem("SizeCost",p_size_total);
+					localStorage.setItem("ToppingsCost",topping_total);
+					localStorage.setItem("TotalCost", p_size_total + topping_total );
+				}
+			
+				
+				var form = document.getElementById('pizzaForm');
+			
+				var el = document.getElementById('p_toppings');
+			
+				
+				var tops = el.getElementsByTagName('input');
+			//gets all checked toppings
+				for (var i=0, len=tops.length; i<len; i++) {
+					if ( tops[i].type === 'checkbox' ) {
+						tops[i].onclick = getToppingsTotal;
+					}
+				}
+				var sz = form.elements['p_size'];
+				
+				for (var i=0, len=sz.length; i<len; i++) {
+					sz[i].onclick = getSizePrice;
+				}
+				
+				
+				form.elements['p_size_total'].value = formatDecimal( parseFloat( getRadioVal(form, 'p_size') ) );
+				updatePizzaTotal(form);
+				
+				
+			}());
+			  
